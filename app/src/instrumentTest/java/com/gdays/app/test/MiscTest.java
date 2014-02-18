e7bd597b1761d17b78a121e20055ff1be6610b19
elimence <elimence@gmail.com>
2014-02-18 06:55:58 +0000

    added log message testing

--- app/src/instrumentTest/java/com/gdays/app/test/MiscTest.java
+++ app/src/instrumentTest/java/com/gdays/app/test/MiscTest.java
@@ -2,10 +2,8 @@ package com.gdays.app.test;
 
 import android.test.ActivityInstrumentationTestCase2;
 
-import com.gdays.app.R;
-import com.gdays.app.Settings_;
-import com.robotium.solo.*;
 import com.gdays.app.MainActivity_;
+import com.robotium.solo.Solo;
 
 public class MiscTest extends
         ActivityInstrumentationTestCase2<MainActivity_> {
@@ -26,41 +24,17 @@ public class MiscTest extends
     }
 
     public void testRun() {
+        int timeout = 5;
 
-        solo.clickOnMenuItem("Settings");
-        solo.sleep(1000); // give it time to change activity
-        solo.assertCurrentActivity("some message", Settings_.class);
+       // TODO
+       // Print out log messages in the Settings Activity and
+       // use robotium to check that they are printed
+       // an example follows
 
-        solo.clearEditText((android.widget.EditText) solo
-                .getView(R.id.responder_text));
-
-        solo.enterText((android.widget.EditText) solo
-                .getView(R.id.responder_text), "test");
-        // Hide the soft keyboard
-        solo.hideSoftKeyboard();
-
-        // Click on Enter
-        solo.clickOnView(solo.getView(R.id.btn_save_text));
-        // Wait for Toast to appear
-        assertTrue("Failed to save edits", solo.searchText("Response text saved"));
-
-
-        // Click on SWitch to deactivate
-        solo.clickOnView(solo.getView(R.id.responder));
-        solo.waitForText("Auto responder deactivated");
-        // Click second time to activate
-        solo.clickOnView(solo.getView(R.id.responder));
-        assertTrue("Failed to toggle switch", solo.searchText("Auto responder active"));
-
-        // Go back to MainActivity_
-        solo.goBack();
-        // Start Settings_ again
-        solo.clickOnMenuItem("Settings");
-        solo.sleep(1000); // give it time to change activity
-        // Check if text change persisted
-        assertTrue("Text failed to persist after change to 'test'", solo.waitForView(solo
-                .getView(R.id.responder_text)));
-
-
+        // Assert that: Log Message 'Entered Settings Activity' is shown
+        assertTrue(
+                "Log message - 'Entered Settings Activity' is not shown!",
+                solo.waitForLogMessage("Entered Settings Activity",
+                        timeout));
     }
 }
